1a08f667f0dbc8f8a921d442497ca7e09f2e48c6
2eg4rizve <2eg4rizve.com>
2023-10-06 23:27:41 +0600

    About Section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,8 +2,29 @@
 
 const About = () => {
     return (
-        <div>
-            About Page
+        <div className="my-[50px]">
+            <h1 className="text-5xl font-bold text-center my-[20px]">ABOUT</h1>
+            <p>
+
+                Our event management service for social events is a comprehensive and personalized solution that will help you to create a memorable and enjoyable experience for your guests. I specialize in planning and executing weddings, birthday parties, anniversaries, engagement parties, retirement parties, and baby showers.
+
+                We understand that each event is unique, and I will work closely with you to develop a plan that reflects your personal style and budget. I will also take care of all the details, from booking the venue and vendors to coordinating the ceremony and reception.
+
+
+            </p>
+            <br />
+
+            <p className="font-bold  my-[20px]">
+                Here are some of the benefits of using our event management service:
+                <br />
+                <br />
+
+                We have extensive experience in planning and executing social events.<br />
+                We are a creative and innovative event planner who can bring your vision to life.<br />
+                We are detail-oriented and organized, and I will ensure that everything runs smoothly on the day of your event.<br />
+                We are flexible and accommodating, and I will work with you to create an event that meets your specific needs.<br />
+                We are affordable and offer a variety of packages to fit your budget.<br />
+            </p>
         </div>
     );
 };
