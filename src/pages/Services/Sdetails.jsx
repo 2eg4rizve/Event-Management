06f5b2caee083a05dd4d120956e7f5a4707ac134
2eg4rizve <2eg4rizve.com>
2023-10-07 16:22:41 +0600

    Features

--- src/pages/Services/Sdetails.jsx
+++ src/pages/Services/Sdetails.jsx
@@ -9,6 +9,10 @@ import 'swiper/css/navigation';
 
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import Speciality from '../../components/Speciality/Speciality';
+
+
+import Marquee from "react-fast-marquee";
 
 const Sdetails = () => {
 
@@ -32,7 +36,7 @@ const Sdetails = () => {
 
     console.log("Find Service ", findService);
 
-    const { name, images, price, short_description, long_description } = findService || {};
+    const { name, images1, images2, images3, price, short_description, long_description, features } = findService || {};
 
     // const firstImage = images.length > 0 ? images[0] : null;
     // console.log(images.length)
@@ -50,6 +54,18 @@ const Sdetails = () => {
 
             </div>
 
+            <div className='flex mb-[30px]  mt-[30px]'>
+                <button className='btn btn-primary'>Contact Us</button>
+                <Marquee pauseOnHover={true} speed={150}>
+                    <p className='text-green-500 mr-[50px]'>Mobile : 01877777777</p>
+                    <p className='text-green-500 mr-[50px]'>Mobile : 01988888888</p>
+                    <p className='text-green-500 mr-[50px]'>Mobile : 01766666666</p>
+                    <p className='text-green-500 mr-[50px]'>Email : Riz@gmail.com</p>
+                    <p className='text-green-500 mr-[50px]'>Facebook : Riz@gmail.com</p>
+                    <p className='text-green-500 mr-[50px]'>Address : Halishahar, Chittagong</p>
+                </Marquee>
+            </div>
+
             <p className="mt-[50px] text-center" >{long_description}</p>
 
 
@@ -58,33 +74,50 @@ const Sdetails = () => {
 
 
             <div>
-            <Swiper
-                style={{
-                    "--swiper-pagination-color": "red",
-                    "--swiper-navigation-color": "green",
-                }}
-                spaceBetween={30}
-                slidesPerView={1}
-                loop={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="mySwiper"
+                <Swiper
+                    style={{
+                        "--swiper-pagination-color": "red",
+                        "--swiper-navigation-color": "green",
+                    }}
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    loop={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
 
-            >
-                <SwiperSlide><img className='h-[400px] w-full' src="/public/Banner/b1.jpg" alt="" /></SwiperSlide>
-                <SwiperSlide><img className='h-[400px] w-full' src="/public/Banner/b2.jpg" alt="" /></SwiperSlide>
-                <SwiperSlide><img className='h-[400px] w-full' src="/public/Banner/b3.jpg" alt="" /></SwiperSlide>
-               
+                >
+                    <SwiperSlide><img className='h-[400px] w-full' src={images1} alt="" /></SwiperSlide>
+                    <SwiperSlide><img className='h-[400px] w-full' src={images2} alt="" /></SwiperSlide>
+                    <SwiperSlide><img className='h-[400px] w-full' src={images3} alt="" /></SwiperSlide>
 
 
-            </Swiper>
+
+                </Swiper>
+
+                {/* <div className='text-center'>
+                    <Speciality></Speciality>
+                </div> */}
+            </div>
+
+
+            <div>
+                <h1 className='text-center text-5xl font-bold my-[50px]'> Features</h1>
+
+                <p className='font-semibold'>
+                    {features}
+                </p>
+            </div>
+
+            <div>
+                <Speciality></Speciality>
             </div>
 
 
@@ -93,7 +126,7 @@ const Sdetails = () => {
 
 
 
-         
+
 
 
 
